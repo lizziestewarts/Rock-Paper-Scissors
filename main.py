b55e9c3491a3d7b720249d4797f7127647044a03
lizziestewarts <elizabeth.stewartnz@gmail.com>
2024-09-03 15:50:05 +1200

    Defined play_game() to run main game loop, and added get_user_choice() for user input

--- main.py
+++ main.py
@@ -10,3 +10,27 @@ Date started: 3/9/24
 Last updated: 3/9/24
 """
 
+import random  #import random for generating random choices for the computer
+
+def get_user_choice():
+    """
+    Get user's choice of rock, paper, or scissors.
+    Returns:
+        str: user's choice (needs to be 'rock', 'paper', or )
+    """
+    user_input = input("Enter rock, paper, scissors: ").lower() #convert input to lowercase to handle case sensitivity
+    if user_input in ["rock", "paper", "scissors"]:
+        return user_input #valid input
+    else:
+        print("Invalid input. Try again.")
+        return get_user_choice() #recursive call until valid input
+
+
+def play_game():
+    """
+    Main game loop
+    """
+    user_choice = get_user_choice() #get user's choice
+
+if __name__ == "__main__":
+    play_game() #run game if this file is executed directly
