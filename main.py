084cbf3ca6d630d205f1cbcf2916ce9fbdeccaf2
lizziestewarts <elizabeth.stewartnz@gmail.com>
2024-09-03 15:30:27 +1200

    Starting main.py with header comment

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+Rock-Paper-Scissors Game
+
+This is a simple command-line implementation of the classic Rock-Paper-Scissors game.
+Players can compete against the computer by selecting 'rock', 'paper', or 'scissors'.
+The game determines the winner based on standard rules and displays the result.
+
+Author: Elizabeth Stewart
+Date started: 3/9/24
+Last updated: 3/9/24
+"""
+
